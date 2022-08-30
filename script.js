7c13222b64824104bba1505cd3495c6d6a8f3db2
PedroGois <pedromorais.gois@gmail.com>
2022-08-30 18:15:59 -0300

    Direções para a cobrinha

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let context = canvas.getContext("2d")
 let box = 32;
 let snake = [];
 snake[0] = {x: 8 * box, y: 8 * box}
+
 let direction = "right";
 
 function criarBG(){
@@ -17,6 +18,15 @@ function criarSnake(){
     }   
 }
 
+document.addEventListener('keydown',update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function IniciarJogo(){
     criarBG();
     criarSnake();
@@ -25,14 +35,16 @@ function IniciarJogo(){
 
     if (direction == "right") snakeX +=box;
     if (direction == "left") snakeX -=box;
-    if (direction == "up") snakeY +=box;
-    if (direction == "down") snakeY -=box;
+    if (direction == "up") snakeY -=box;
+    if (direction == "down") snakeY +=box;
 
     snake.pop();
+
     let newHead = {
         x:snakeX , y: snakeY
-    };
-    snake.unshift(newHead)
+    }
+
+    snake.unshift(newHead);
 }
 
 let jogo = setInterval(IniciarJogo,100);

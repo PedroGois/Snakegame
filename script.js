bf1bf2c04232f12a0c273589ee5cce8403086b6c
PedroGois <pedromorais.gois@gmail.com>
2022-08-31 17:03:50 -0300

    colisão no proprio corpo da cobrinha

--- script.js
+++ script.js
@@ -41,6 +41,13 @@ function IniciarJogo(){
     if (snake[0].y > 15 * box && direction=="down")snake[0].y = 0;
     if (snake[0].y < 0 && direction=="up")snake[0].y = 16 * box;
 
+    for (i = 1; i < snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Você perdeu :(");
+        }
+    }
+
     criarBG();
     criarSnake();
     drawfood();    
@@ -68,4 +75,4 @@ function IniciarJogo(){
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(IniciarJogo,200);
+let jogo = setInterval(IniciarJogo,150);

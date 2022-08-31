4828f047d79605c54f8d1e82870fc4e15e429ddc
PedroGois <pedromorais.gois@gmail.com>
2022-08-31 16:56:33 -0300

    definindo efeito cobra come comida

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ let box = 32;
 let snake = [];
 snake[0] = {x: 8 * box, y: 8 * box}
 
-let direction = "right";
+let direction = "up";
 let food = {
     x:Math.floor(Math.random() * 16) * box,
     y:Math.floor(Math.random() * 16) * box
 }
 
 function criarBG(){
-    context.fillStyle = "Black";
+    context.fillStyle = "black";
     context.fillRect (0, 0, 16*box, 16*box)
 }
 
 function criarSnake(){
-    for (i=0; i<snake.length; i++){
+    for (i=0; i < snake.length; i++){
         context.fillStyle="red";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }    
@@ -36,9 +36,15 @@ function update(event){
 }
 
 function IniciarJogo(){
+    if (snake[0].x > 15 * box && direction=="right")snake[0].x = 0;
+    if (snake[0].x < 0 && direction=="left")snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction=="down")snake[0].y = 0;
+    if (snake[0].y < 0 && direction=="up")snake[0].y = 16 * box;
+
     criarBG();
     criarSnake();
-    ;
+    drawfood();    
+    
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
@@ -46,8 +52,14 @@ function IniciarJogo(){
     if (direction == "left") snakeX -=box;
     if (direction == "up") snakeY -=box;
     if (direction == "down") snakeY +=box;
-
-    snake.pop();
+    
+    if (snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
 
     let newHead = {
         x:snakeX , y: snakeY
@@ -56,4 +68,4 @@ function IniciarJogo(){
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(IniciarJogo,100);
+let jogo = setInterval(IniciarJogo,200);
